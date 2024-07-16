322de0044099ec22164ee5db2e26008f11fc8d24
Dávid Besenyei <37157280+Besi97@users.noreply.github.com>
2024-07-16 21:36:00 +0200

    Implement mutations (#3)
    Implement basic mutations for testing

--- src/main/kotlin/dev/besi/inventory/backend/mapper/ProductMapper.kt
+++ src/main/kotlin/dev/besi/inventory/backend/mapper/ProductMapper.kt
@@ -1,10 +1,13 @@
 package dev.besi.inventory.backend.mapper
 
 import dev.besi.inventory.backend.repository.model.Product
+import dev.besi.inventory.graphql.model.ProductMutation
 import org.mapstruct.Mapper
 
 @Mapper(componentModel = "spring")
 interface ProductMapper {
     fun map(product: Product): dev.besi.inventory.graphql.model.Product
     fun map(products: List<Product>): List<dev.besi.inventory.graphql.model.Product>
+
+    fun mapToDbModel(product: ProductMutation): Product
 }
--- src/main/kotlin/dev/besi/inventory/backend/resolver/DemoResolver.kt
+++ src/main/kotlin/dev/besi/inventory/backend/resolver/DemoResolver.kt
@@ -2,17 +2,36 @@ package dev.besi.inventory.backend.resolver
 
 import dev.besi.inventory.backend.mapper.ProductMapper
 import dev.besi.inventory.backend.repository.model.ProductRepository
+import dev.besi.inventory.graphql.api.CreateProductMutationResolver
 import dev.besi.inventory.graphql.api.ProductsQueryResolver
+import dev.besi.inventory.graphql.api.UpdateProductMutationResolver
 import dev.besi.inventory.graphql.model.Product
+import dev.besi.inventory.graphql.model.ProductMutation
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import java.math.BigDecimal
 
 @Controller
 class DemoResolver(
     val productRepository: ProductRepository,
     val productMapper: ProductMapper
-): ProductsQueryResolver {
+): ProductsQueryResolver, CreateProductMutationResolver, UpdateProductMutationResolver {
     @QueryMapping
     override fun products(): List<Product> =
         productMapper.map(productRepository.findAll().toList())
+
+    override fun createProduct(newProduct: ProductMutation): Product =
+        productRepository.save(productMapper.mapToDbModel(newProduct))
+            .let { productMapper.map(it) }
+
+    override fun updateProduct(productId: String, product: ProductMutation): Product =
+        productRepository.findById(productId.toLong())
+            .map {
+                it.name = product.name
+                it.price = BigDecimal(product.price)
+                return@map productRepository.save(it)
+            }
+            .map { productMapper.map(it) }
+            .orElseThrow { EntityNotFoundException("Product not found with ID: $productId") }
 }
