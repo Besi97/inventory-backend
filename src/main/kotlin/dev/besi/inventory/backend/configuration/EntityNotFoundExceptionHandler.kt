c96c331aaf6a9945c41bae290ffc5ef0ad67a01d
Besi <david.besenyei97@gmail.com>
2024-07-18 19:53:14 +0200

    create exception handler

---
+++ src/main/kotlin/dev/besi/inventory/backend/configuration/EntityNotFoundExceptionHandler.kt
@@ -0,0 +1,20 @@
+package dev.besi.inventory.backend.configuration
+
+import graphql.GraphQLError
+import graphql.GraphqlErrorBuilder
+import graphql.schema.DataFetchingEnvironment
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter
+import org.springframework.graphql.execution.ErrorType
+import org.springframework.stereotype.Component
+
+@Component
+class EntityNotFoundExceptionHandler: DataFetcherExceptionResolverAdapter() {
+    override fun resolveToSingleError(ex: Throwable, env: DataFetchingEnvironment): GraphQLError? =
+        if (ex is EntityNotFoundException)
+            GraphqlErrorBuilder.newError(env)
+                .errorType(ErrorType.NOT_FOUND)
+                .message(ex.message)
+                .build()
+        else null
+}
